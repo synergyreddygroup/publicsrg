3375e8fc67412250ed632c33591691f931621fcf
synergyreddygroup <synergyreddygroup@gmail.com>
2025-02-04 10:18:19 +0530

    Delete src directory

--- src/utlis/ProtectedRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-
-const ProtectedRoutes = () => {
-    const user = true
-    return user ? <Outlet /> : <Navigate to="https://publicsrg.vercel.app/"/>
-}
-
-export default ProtectedRoutes;
